c31535ae1f4003bbecab6400ce9901d8ce59a33f
ankan_cs <ankan.bhattacharyya.94@gmail.com>
2023-05-18 18:33:18 -0400

    Update vec3.h

--- vec3.h
+++ vec3.h
@@ -20,7 +20,7 @@ class vec3{
         double operator[](int i) const {return e[i];} // Index operator: (1,0,1)[1]=0. It returns a copy of e[i]. So, any change/modification to the output does not affect the original data.
         double& operator[](int i) {return e[i];} // Index operator: (1,0,1)[1]=0. Returning reference to e[i]. So, any change, will modify. So, the return is not const.
 
-        vec3& operator+=(const vec3 &v){ // (1,0,1) += (1,0,1) = (2,0,2)
+        vec3& operator+=(const vec3 &v){ // (1,0,1) += (1,0,1) = (2,0,2) or (2,0,2) = (1,0,1) + (1,0,1)
             e[0] += v.e[0];
             e[1] += v.e[1];
             e[2] += v.e[2];
@@ -59,3 +59,49 @@ using point3 = vec3; // 3D point
 using color = vec3; // RGB color
 
 #endif
+
+// vec3 Utility Functions
+
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &v) {
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t) {
+    return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t) {
+    return (1/t) * v;
+}
+
+inline double dot(const vec3 &u, const vec3 &v) {
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
